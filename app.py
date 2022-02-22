3f1bf7ff028de14871c26046c548e7ec0146bcaa
Abesh Jha <mcs212739@cse.iitd.ac.in>
2022-02-22 15:29:14 +0545

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="suppliers",
+    user="postgres",
+    password="Abcd1234")
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p><center><h1>SECOND HAND CAR SELLING MARKETPLACE</h1></center></p>"
