a4c3af05d9fd35bb72c62620f338263df21f7a0d
Abesh jha <magicianabesh123@gmail.com>
2022-02-25 22:47:08 +0545

    homepage slider done

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template,request,redirect
 import psycopg2
 import os
 
+#set FLASK_ENV=development
+#psql --host=john.db.elephantsql.com --port=5432 --username=oxcbzzsv  --password --dbname=oxcbzzsv
+#G0UGzkGcZ6X8OH_di8lpyT8REJ3nl6Qv
 
 def get_db_connection():
     con=psycopg2.connect(
@@ -17,25 +20,67 @@ app = Flask(__name__,template_folder='FRONT_END')
 
 @app.route("/")
 def homepage():
-    postgreSQL_select_Query = "select * from brand LIMIT 3"
+    advertisement_query = "select * from advertisements ORDER BY random() LIMIT 3"
+    brand_query = "select * from brand ORDER BY random() LIMIT 3"
+    model_query = "select * from model ORDER BY random() LIMIT 3"
+    con= get_db_connection()
+    cursor=con.cursor()
+
+    cursor.execute(advertisement_query)
+    advertisements = cursor.fetchall()
+
+    cursor.execute(brand_query)
+    brands = cursor.fetchall()
+    
+    cursor.execute(model_query)
+    models = cursor.fetchall()
+    
+    print(models)
+    print(brands)
+    return render_template('index.html',advertisements=advertisements,models=models,brands=brands)
+
+@app.route("/models")
+def models():
+    postgreSQL_select_Query = "select * from model"
     con= get_db_connection()
     cursor=con.cursor()
     cursor.execute(postgreSQL_select_Query)
-    print("Selecting rows from mobile table using cursor.fetchall ")
+    print("Selecting all models ")
     models = cursor.fetchall()
 
     print("Print each row and it's columns values")
-    return render_template('index.html',models=models)
+    return render_template('models.html',models=models)
+
+@app.route("/advertisement")
+def advertisement():
+    postgreSQL_select_Query = "select * from advertisements LIMIT 100"
+    con= get_db_connection()
+    cursor=con.cursor()
+    cursor.execute(postgreSQL_select_Query)
+    print("Selecting all brands ")
+    items = cursor.fetchall()
+    print("Print each row and it's columns values")
+    return render_template('advertisement.html',items=items)
+
+@app.route("/brands")
+def brands():
+    postgreSQL_select_Query = "select * from brand"
+    con= get_db_connection()
+    cursor=con.cursor()
+    cursor.execute(postgreSQL_select_Query)
+    print("Selecting all brands ")
+    brands = cursor.fetchall()
+    print("Print each row and it's columns values")
+    return render_template('brands.html',brands=brands)
 
 @app.route("/test")
 def test():
-    postgreSQL_select_Query = "select * from brand LIMIT 3"
+    postgreSQL_select_Query = "select * from advertisements LIMIT 3"
     con= get_db_connection()
     cursor=con.cursor()
     cursor.execute(postgreSQL_select_Query)
     print("Selecting rows from mobile table using cursor.fetchall ")
     models = cursor.fetchall()
-
     print("Print each row and it's columns values")
     return render_template('test.html',models=models)
    
@@ -44,10 +89,6 @@ def test():
 def login():
     return render_template('login.html')
 
-# @app.route("/signup")
-# def signup():
-#     s=NULL
-#     return render_template('signup.html',mssg=s)
 
 @app.route("/login_handle", methods=['GET', 'POST'])
 def login_handle():
